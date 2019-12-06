eea27448393b1f9768a49590be71c23d696dc844
wisdomroc <r.o.c.wang@163.com>
2019-12-06 15:07:29 +0800

    first commit

---
+++ mytablemodel.cpp
@@ -0,0 +1,37 @@
+﻿#include "mytablemodel.h"
+
+MyTableModel::MyTableModel()
+{
+
+}
+
+int MyTableModel::rowCount(const QModelIndex &parent) const
+{
+    Q_UNUSED(parent)
+    return m_cepriInfoList.count();
+}
+
+int MyTableModel::columnCount(const QModelIndex &parent) const
+{
+
+}
+
+QVariant MyTableModel::data(const QModelIndex &index, int role) const
+{
+
+}
+
+QVariant MyTableModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+
+}
+
+bool MyTableModel::setData(const QModelIndex &index, const QVariant &value, int role)
+{
+
+}
+
+Qt::ItemFlags MyTableModel::flags(const QModelIndex &index) const
+{
+
+}
---
+++ mytablemodel.h
@@ -0,0 +1,32 @@
+﻿#ifndef MYTABLEMODEL_H
+#define MYTABLEMODEL_H
+
+#include <QObject>
+#include <QAbstractTableModel>
+
+class MyTableModel : public QAbstractTableModel
+{
+    Q_OBJECT
+
+    struct CepriInfo {
+        QString name;
+        int value;
+        QString detailInfo;
+    };
+
+public:
+    MyTableModel();
+
+protected:
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    int columnCount(const QModelIndex &parent = QModelIndex()) const override;
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
+    bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole) override;
+    Qt::ItemFlags flags(const QModelIndex &index) const override;
+
+private:
+    QList<CepriInfo> m_cepriInfoList;
+};
+
+#endif // MYTABLEMODEL_H
